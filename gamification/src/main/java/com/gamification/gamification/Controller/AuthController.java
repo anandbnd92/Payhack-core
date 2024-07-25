79e3ae5ae4e372381971291e407e55deae026f2c
Rani <raniguthula59@gmail.com>
2024-07-25 17:20:55 +0530

    login issues resolved

--- gamification/src/main/java/com/gamification/gamification/Controller/AuthController.java
+++ gamification/src/main/java/com/gamification/gamification/Controller/AuthController.java
@@ -2,6 +2,7 @@ package com.gamification.gamification.Controller;
 
 import com.gamification.gamification.Entity.User;
 import com.gamification.gamification.Repository.UserRepository;
+import com.gamification.gamification.util.JwtUtil;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,6 +30,9 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -36,42 +40,28 @@ public class AuthController {
         return ResponseEntity.ok().body("User registered successfully");
     }
 
-//        @PostMapping("/userlogin")
-//    public ResponseEntity<?> login(@RequestBody User user) {
-//        System.out.println(userRepository.findByUsername(user.getUsername()));
-//        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(user.getUsername()));
-//
-//
-//        try {
-//
-//            Authentication authentication = authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-//            );
-//
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//            return ResponseEntity.ok(new LoginResponse("User authenticated successfully", user.getUsername()));
-//        } catch (AuthenticationException e) {
-//            // If authentication fails, return an error response
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-//        }
-//    }
-//    @PostMapping("/userlogin")
-//    public ResponseEntity<?> login(@RequestBody User user) {
-//
-////        String username = user.getUsername();
-//        String isuser = userRepository.isuserExist(user.getUsername());
-//        System.out.println("isuser" + isuser);
-//
-//        if (isuser != null) {
-//            // User exists, return success response
-//            return ResponseEntity.ok("User exists");
-//        } else {
-//            // User does not exist, return error response
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User does not exist");
-//        }
-//    }
+    @PostMapping("/userlogin")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        System.out.println(userRepository.findByUsername(user.getUsername()));
+        Optional<User> userOptional = Optional.ofNullable(userRepository.findByUsername(user.getUsername()));
 
+
+        try {
+
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String jwt = jwtUtil.generateToken(user.getUsername());
+
+            return ResponseEntity.ok(new LoginResponse("User authenticated successfully", jwt));
+        } catch (AuthenticationException e) {
+            // If authentication fails, return an error response
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
 
 }
 
