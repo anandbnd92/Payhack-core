24c07df0f5fb0a2a7173f99a9233153b53ba861d
vishal <vishalnimcet@gmail.com>
2024-07-25 14:58:14 +0530

    initial commit

--- gamification/src/main/java/com/gamification/gamification/Controller/QuestionController.java
+++ gamification/src/main/java/com/gamification/gamification/Controller/QuestionController.java
@@ -1,25 +1,24 @@
 package com.gamification.gamification.Controller;
 
-import com.gamification.gamification.Entity.Question;
-import com.gamification.gamification.Entity.QuestionDto;
-import com.gamification.gamification.Service.QuestionService;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
+import com.gamification.gamification.Entity.QuestionDto;
+import com.gamification.gamification.Service.QuestionService;
 
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class QuestionController {
 
-        @Autowired
-        private QuestionService questionService;
+    @Autowired
+    private QuestionService questionService;
 
     @GetMapping("/subjects")
     public List<Map<String,String>> getquizs()
