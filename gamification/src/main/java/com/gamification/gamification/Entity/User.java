321b23292873a987bc93348b9076093ad1d5f9c1
Rani <raniguthula59@gmail.com>
2024-07-25 21:00:30 +0530

    added the condition if user doesn't exists and changes response pattern

--- gamification/src/main/java/com/gamification/gamification/Entity/User.java
+++ gamification/src/main/java/com/gamification/gamification/Entity/User.java
@@ -1,59 +1,46 @@
-package com.gamification.gamification.Entity;
-
-
-import jakarta.persistence.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "users")
-@EntityListeners(AuditingEntityListener.class)
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
+//import jakarta.persistence.*;
 //
-//    @Column(nullable = false)
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "users")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String username;
+//    private String password;
 //    private boolean enabled;
-
-//    @Column(nullable = false)
-//    private Timestamp loggedinat;
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+//
+//    @Column(name = "loggedInAt")
+//    private LocalDateTime loggedInAt;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
 //    public boolean isEnabled() {
 //        return enabled;
 //    }
@@ -61,12 +48,12 @@ public class User {
 //    public void setEnabled(boolean enabled) {
 //        this.enabled = enabled;
 //    }
-
-//    public Timestamp getLoggedinat() {
-//        return loggedinat;
+//
+//    public LocalDateTime getLoggedInAt() {
+//        return loggedInAt;
 //    }
 //
-//    public void setLoggedinat(Timestamp loggedinat) {
-//        this.loggedinat = loggedinat;
+//    public void setLoggedInAt(LocalDateTime loggedInAt) {
+//        this.loggedInAt = loggedInAt;
 //    }
-}
+//}
